4fd67a3bb1660dd23aee0dc9b6fa6beb0b1471ab
hoangks5 <hoangnd.ute@gmail.com>
2022-10-06 11:59:18 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gretel
 import pandas as pd
 
-data_source = 'https://gretel-public-website.s3.amazonaws.com/datasets/credit-timeseries-dataset.csv'
+data_source = 'AAPL Historical Data.csv'
 original_df = pd.read_csv(data_source)
 original_df
