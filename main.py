4d99af8a8a9b60f1e1c43ed6d52a143e007fcca9
hoangks5 <hoangnd.ute@gmail.com>
2022-10-10 14:49:31 +0700

    update

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ data_source = 'AAPL Historical Data.csv'
 os.mkdir(data_source.split('.')[0])
 data_original = pd.read_csv(data_source)
 for i in range(30):
-    
+    data = data_original['Price']
+    for index in range(len(data)):
+        data[index] += data[index]*random.randint(-30,30)/100
+    data_original['Price'] = data
+    data_original.to_csv('./train.csv',index=False)
     
