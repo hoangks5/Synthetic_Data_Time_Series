3d3421348ace57c6c1d626846c623d49e43e3c88
hoangks5 <hoangnd.ute@gmail.com>
2022-10-10 14:48:21 +0700

    update

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 import pandas as pd
 import os
 
-
+### Create file csv data from sample
 data_source = 'AAPL Historical Data.csv'
 os.mkdir(data_source.split('.')[0])
 data_original = pd.read_csv(data_source)
 for i in range(30):
+    
     
