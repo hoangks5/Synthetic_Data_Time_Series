b618cb9186a458da8ec7da1f1b5419a043433480
hoangks5 <hoangnd.ute@gmail.com>
2022-10-10 14:48:11 +0700

    update

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 import pandas as pd
 import os
+
+
 data_source = 'AAPL Historical Data.csv'
 os.mkdir(data_source.split('.')[0])
-original_df = pd.read_csv(data_source)
+data_original = pd.read_csv(data_source)
 for i in range(30):
     
