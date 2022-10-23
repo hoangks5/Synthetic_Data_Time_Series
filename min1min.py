6bbbbeb03e85c16d0c1f8f9626783fbb419687b3
hoangks5 <hoangnd.ute@gmail.com>
2022-10-23 19:12:47 +0700

    update

--- min1min.py
+++ min1min.py
@@ -1,11 +1,4 @@
-from instagrapi import Client
-
-cl = Client()
-cl.login('hoangkss5', 'Hoang22041999')
-
-media_pk = cl.media_pk_from_url('https://www.instagram.com/p/ChLtDjWjmQ9/')
-media_path = cl.video_download(media_pk)
-
-
-
-
+from instapy import InstaPy
+  
+session = InstaPy(username="your username",password="your password")
+session.login()
