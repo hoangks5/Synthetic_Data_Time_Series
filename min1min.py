5f833503bdbf44dd377a116de2e8084d17584ac1
hoangks5 <hoangnd.ute@gmail.com>
2022-10-23 19:12:57 +0700

    update

--- min1min.py
+++ min1min.py
@@ -1,4 +1,4 @@
 from instapy import InstaPy
   
-session = InstaPy(username="your username",password="your password")
+session = InstaPy(username="hoangkss5",password="Hoang22041999")
 session.login()
