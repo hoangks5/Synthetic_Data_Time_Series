38a44c4e72c43ea5a35c2c484a6728f536beb128
hoangks5 <hoangnd.ute@gmail.com>
2022-10-23 19:03:37 +0700

    update

--- min1min.py
+++ min1min.py
@@ -3,5 +3,9 @@ from instagrapi import Client
 cl = Client()
 cl.login('hoangkss5', 'Hoang22041999')
 
-user_id = cl.user_id_from_username("nobita_mi")
-medias = cl.user_medias(user_id, 20)
+media_pk = cl.media_pk_from_url('https://www.instagram.com/p/CO8JG34gxc32HxGOxB-mw_FEQ59P8MbFDYTJAc0/')
+media_path = cl.video_download(media_pk)
+
+
+
+
