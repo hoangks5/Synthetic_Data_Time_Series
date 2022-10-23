56f1df40834ac6c5902137e2972c84233c297fb6
hoangks5 <hoangnd.ute@gmail.com>
2022-10-23 19:00:07 +0700

    update

--- min1min.py
+++ min1min.py
@@ -3,5 +3,5 @@ from instagrapi import Client
 cl = Client()
 cl.login('hoangkss5', 'Hoang22041999')
 
-user_id = cl.user_id_from_username("adw0rd_")
+user_id = cl.user_id_from_username("nobita_mi")
 medias = cl.user_medias(user_id, 20)
