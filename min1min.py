bc5bf2ddfc6eb73df973155922912e737cd7973d
hoangks5 <hoangnd.ute@gmail.com>
2022-10-23 19:04:47 +0700

    update

--- min1min.py
+++ min1min.py
@@ -3,7 +3,7 @@ from instagrapi import Client
 cl = Client()
 cl.login('hoangkss5', 'Hoang22041999')
 
-media_pk = cl.media_pk_from_url('https://www.instagram.com/p/CO8JG34gxc32HxGOxB-mw_FEQ59P8MbFDYTJAc0/')
+media_pk = cl.media_pk_from_url('https://www.instagram.com/p/ChLtDjWjmQ9/')
 media_path = cl.video_download(media_pk)
 
 
