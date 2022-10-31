d753a2eedb045bbea4eea8b38434e20179f0c4bb
hoangks5 <hoangnd.ute@gmail.com>
2022-10-31 12:24:51 +0700

    Git Commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 data_source = 'website_classification.csv'
 
 w = open('./data/data(1-4).csv','r',encoding='utf-8').read().splitlines()
-for i in w:
+for i in w[50]:
     a = i.split(',')[1]
     json_new = """{"website": "<iframe src='"""+a+"""'width='100%' height='600px'/>"}"""
     
