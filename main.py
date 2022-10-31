354c6342ac6740aabddf6b4f2407bbcda6703749
hoangks5 <hoangnd.ute@gmail.com>
2022-10-31 11:48:00 +0700

    Git Commit

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ folder = pd.read_csv(data_source,index_col=False)
 
 with open(data_source,'r',encoding='utf-8') as f:
     st = f.read()
+s = st.splitlines()
+if i in s:
+    t = i.split(',')
+    print(t[1])
     
             
