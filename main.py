3018e53a7450ac70e6cb7c9330fd0b4691b3f371
hoangks5 <hoangnd.ute@gmail.com>
2022-10-31 11:49:30 +0700

    Git Commit

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import pandas as pd
 import os
 data_source = 'website_classification.csv'
 
-
+frame_new = ['Travel',
+ 'Social Networking and Messaging',
+ 'News',
+ 'Streaming Services']
 with open(data_source,'r',encoding='utf-8') as f:
     st = f.read()
 s = st.splitlines()
