efc933095e0f1d9f0bf1106eeca7040709ed0b6c
hoangks5 <hoangnd.ute@gmail.com>
2022-10-31 11:48:20 +0700

    Git Commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
 import pandas as pd
 import os
-folder = pd.read_csv(data_source,index_col=False)
+data_source = 'website_classification.csv'
+
 
 with open(data_source,'r',encoding='utf-8') as f:
     st = f.read()
